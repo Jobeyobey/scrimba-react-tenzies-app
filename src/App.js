702cee134ad32c162065973b8262706b02822505
Jobeyobey <117482381+Jobeyobey@users.noreply.github.com>
2022-12-21 12:26:50 +0000

    Grid working - Need style
    Got CSS grid working, just need to style the components correctly now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react"
+import Die from "./components/Die"
 
 export default function App() {
+
     return (
         <main>
+            <div className="dice-holder">
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+                <Die value={5} />
+                <Die value={6} />
+                <Die value={1} />
+                <Die value={2} />
+                <Die value={3} />
+                <Die value={4} />
+            </div>
         </main>
     )
 }
+
+/**
+ * Challenge:
+ * 
+ * - Create a Die component that takes a `value` prop
+ * - Render 10 instances of the Die component (manually)
+ *      - Provide a number between 1-6 for the value on each
+ *        for now
+ * - Style the <main> and <Die> components 
+ *   to look like they do in the slide
+ */

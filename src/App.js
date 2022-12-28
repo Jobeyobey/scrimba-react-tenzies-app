8ab5339dd567d178aab41dffcc9e9bd03e5d39f6
Jobeyobey <117482381+Jobeyobey@users.noreply.github.com>
2022-12-28 23:16:02 +0000

    Added title and descriptor

--- src/App.js
+++ src/App.js
@@ -54,6 +54,9 @@ export default function App() {
 
     return (
         <main>
+            <h1 className="title">Tenzies</h1>
+            <p className="description">Roll until all dice are the same. Click each die to
+                freeze it at its current value between rolls.</p>
             <div className="dice-holder">
                 {allDice}
             </div>
